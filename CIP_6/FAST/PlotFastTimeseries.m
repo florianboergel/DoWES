7dfff7ba05ca2dfed2c0fd05d8cb84050b4a2852
Jan <Jan.kaemper@gmail.com>
2016-10-04 14:00:25 +0200

    CIP 6A report

--- CIP_6/FAST/PlotFastTimeseries.m
+++ CIP_6/FAST/PlotFastTimeseries.m
@@ -9,10 +9,10 @@
 %% Main code
 
 % add path with matlab functions
-addpath('C:/Fast/MatlabFunctions');
+addpath('/home/jan/Prive/Semester6/DesignProject/Repo/CIP_6/FAST/MatlabFunctions');
 
 % select FAST output file
-FAST_output_file_name  = select_file_from_folder('C:\Fast\*.out');
+FAST_output_file_name  = select_file_from_folder('/home/jan/Prive/Semester6/DesignProject/Repo/CIP_6/FAST/Fast/*.out');
 
 % create structure in workspace with results
 FAST_results = fast2mat(FAST_output_file_name);
@@ -22,16 +22,16 @@ create_figures = fast2plot(FAST_results);
 
 % calculate damage equivalent loads
 Damage_Eq_Loads = struct;
-% blade root flapwise bending moment (Whöler coefficient = 10)
+% blade root flapwise bending moment (Whï¿½ler coefficient = 10)
 [Damage_Eq_Loads.RootMFlp_Req R]= dequiv(FAST_results.Time, FAST_results.RootMFlp2,10);
 Damage_Eq_Loads.RootMFlp_m = 10;
-% blade root edgewise bending moment (Whöler coefficient = 10)
+% blade root edgewise bending moment (Whï¿½ler coefficient = 10)
 [Damage_Eq_Loads.RootMedg_Req R]= dequiv(FAST_results.Time, FAST_results.RootMedg2,10);
 Damage_Eq_Loads.RootMedg_m = 10;
-% tower base longitudinal (fore-aft) bending moment (Whöler coefficient = 4)
+% tower base longitudinal (fore-aft) bending moment (Whï¿½ler coefficient = 4)
 [Damage_Eq_Loads.TwrBsMyt_Req R]= dequiv(FAST_results.Time, FAST_results.TwrBsMyt,4);
 Damage_Eq_Loads.TwrBsMyt_m = 4;
-% tower base transversal (side-side) bending moment (Whöler coefficient = 4)
+% tower base transversal (side-side) bending moment (Whï¿½ler coefficient = 4)
 [Damage_Eq_Loads.TwrBsMxt_Req R]= dequiv(FAST_results.Time, FAST_results.TwrBsMxt,4);
 Damage_Eq_Loads.TwrBsMxt_m = 4;
 clear R
--- CIP_6/FAST/wind/wake_8d/FAST_batchmode.bat
+++ CIP_6/FAST/wind/wake_8d/FAST_batchmode.bat
@@ -1,4 +1,4 @@
-TurbSim64 "TurbSim_ws_wake5 _8.inp"
-TurbSim64 "TurbSim_ws_wake10 _8.inp"
+TurbSim64 "TurbSim_ws_wake5_8.inp"
+TurbSim64 "TurbSim_ws_wake10_8.inp"
 TurbSim64 "TurbSim_ws_wake15_8.inp"
 TurbSim64 "TurbSim_ws_wake25_8.inp"
